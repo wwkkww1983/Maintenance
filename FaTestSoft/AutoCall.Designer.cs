27c5f3cafa7a1528b464e859077072815ed7d148
Jediws <intelligentest@hotmail.com>
2014-09-29 11:32:40 +0800

    原始

---
+++ FaTestSoft/AutoCall.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FaTestSoft
+{
+    partial class AutoCall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonSingleCall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "自动总招间隔(秒):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "设定";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonSingleCall
+            // 
+            this.buttonSingleCall.Location = new System.Drawing.Point(103, 199);
+            this.buttonSingleCall.Name = "buttonSingleCall";
+            this.buttonSingleCall.Size = new System.Drawing.Size(75, 23);
+            this.buttonSingleCall.TabIndex = 7;
+            this.buttonSingleCall.Text = "单次总召";
+            this.buttonSingleCall.UseVisualStyleBackColor = true;
+            this.buttonSingleCall.Click += new System.EventHandler(this.buttonSingleCall_Click);
+            // 
+            // AutoCall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonSingleCall);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "AutoCall";
+            this.Text = "AutoCall";
+            this.Load += new System.EventHandler(this.AutoCall_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonSingleCall;
+    }
+}
---
+++ FaTestSoft/AutoCall.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FaTestSoft.CommonData;
+
+namespace FaTestSoft
+{
+    public partial class AutoCall : Form
+    {
+        Timer timer;
+        public AutoCall(Timer timer)
+        {
+            InitializeComponent();
+            this.timer = timer;
+        }
+
+        private void AutoCall_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = (timer.Interval / 1000).ToString();
+            if (timer.Enabled == true)
+            {
+                button1.Text = "关闭";
+            }
+            else
+            {
+                button1.Text = "开启";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled == true)
+            {
+                timer.Enabled = false;
+                button1.Text = "开启";
+            }
+            else
+            {
+                timer.Enabled = true;
+                button1.Text = "关闭";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer.Interval = int.Parse(textBox1.Text) * 1000;
+        }
+
+        private void buttonSingleCall_Click(object sender, EventArgs e)
+        {
+            PublicDataClass._NetTaskFlag.C_IC_NA_1 = true;
+            PublicDataClass._GprsTaskFlag.C_IC_NA_1 = true;
+            PublicDataClass._UsbTaskFlag.C_IC_NA_1 = true;
+            PublicDataClass._ComTaskFlag.C_IC_NA_1 = true;
+        }
+    }
+}
